b7f48e67871727d514244c73f8579b19dd86dd96
ralphsulioti <ralph.sulioti@gmail.com>
2024-08-02 13:58:05 -0400

    Added Users, forced sign-in

--- obj/Debug/net8.0/Marketplace.AssemblyInfo.cs
+++ obj/Debug/net8.0/Marketplace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Marketplace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbed54fc124381236daf1e543dd99aadd3de959b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c445cb4b7f20fc5bfc1086f236da1502e5585f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marketplace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marketplace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
